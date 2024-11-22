8f359554f0e731e774c3f4fb0f3e8e4a0141e59b
Davey0076 <davidkwnd@gmail.com>
2024-11-22 13:18:58 +0300

    updates made on favicon

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,10 +5,30 @@ function Contact() {
   return (
     <section id="contact" className="contact">
       <h2>Contact Us</h2>
-      <p>James Njue</p>
-      <p>Email: lordjimlee@gmail.com</p>
-      <p>Phone: +254 707919856</p>
-      <p>Address: 1083, Embu, Nairobi, 60100, Kenya</p>
+      <div className="contact-details">
+        <p>
+          <strong>Name:</strong> James Njue
+        </p>
+        <p>
+          <strong>Email:</strong> <br />
+          <a href="mailto:lordjimlee@gmail.com" className="contact-link">
+            lordjimlee@gmail.com
+          </a>
+        </p>
+        <p>
+          <strong>Phone:</strong> <br />
+          <a href="tel:+254707919856" className="contact-link">
+            +254 707 919 856
+          </a>
+        </p>
+        <p>
+          <strong>Address:</strong> 
+          <p>
+            1083, Embu, Nairobi, 60100, Kenya
+          </p>
+        </p>
+      </div>
+      <p style={{ marginTop: '1rem' }}>Reach out today, and let’s get started on your success journey!</p>
     </section>
   );
 }
