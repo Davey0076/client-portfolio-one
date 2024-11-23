8bdce3c89e3600dea8e613985a64556a9c80f11a
Davey0076 <davidkwnd@gmail.com>
2024-11-23 09:21:32 +0300

    added whatsapp popup

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Guarantee from './components/Guarantee/Guarantee';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 import './App.css';
+import WhatsappPopup from './components/WhatsappPopup/WhatsappPopup'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Contact />
       </main>
       <Footer />
+      <WhatsappPopup />
     </div>
   );
 }
---
+++ src/components/WhatsappPopup/WhatsappPopup.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import './WhatsAppPopup.css';
+import { FaWhatsapp } from 'react-icons/fa'; // React Icons for WhatsApp logo
+
+function WhatsAppPopup() {
+  const [showPopup, setShowPopup] = useState(false);
+
+  useEffect(() => {
+    // Show the popup after 5 seconds
+    const timer = setTimeout(() => {
+      setShowPopup(true);
+    }, 5000);
+
+    return () => clearTimeout(timer); // Cleanup timer on component unmount
+  }, []);
+
+  const handleWhatsAppClick = () => {
+    const userConfirmed = window.confirm(
+      'Do you want to open WhatsApp to start a chat with us?'
+    );
+    if (userConfirmed) {
+      window.open(
+        'https://wa.me/254707919856?text=Hi,%20I%20need%20help%20with%20my%20assignment!',
+        '_blank'
+      );
+    }
+  };
+
+  return (
+    showPopup && (
+      <div className="whatsapp-popup" onClick={handleWhatsAppClick}>
+        <FaWhatsapp className="whatsapp-icon" />
+        <p className="whatsapp-text">WhatsApp</p>
+      </div>
+    )
+  );
+}
+
+export default WhatsAppPopup;
